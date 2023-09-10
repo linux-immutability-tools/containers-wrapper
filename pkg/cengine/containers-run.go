26d4b59f15b7e0b1a9167ee6f6afaf8b947d9767
mirkobrombin <send@mirko.pm>
2023-09-10 13:05:10 +0200

    support for workdir in RunInContainer

--- pkg/cengine/containers-run.go
+++ pkg/cengine/containers-run.go
@@ -1,13 +1,16 @@
 package cengine
 
 // RunInContainer executes a command inside a new container.
-func (ce *Ce) RunInContainer(imageOrId string, interactive bool, command string, extraArgs ...string) (err error) {
+func (ce *Ce) RunInContainer(imageOrId string, interactive bool, command string, workdir string, extraArgs ...string) (err error) {
 	args := []string{"run"}
 	if interactive {
 		args = append(args, "-it")
 	}
 	args = append(args, "--rm")
 	args = append(args, "--entrypoint", command)
+	if workdir != "" {
+		args = append(args, "--workdir", workdir)
+	}
 	args = append(args, imageOrId)
 	args = append(args, extraArgs...)
 
