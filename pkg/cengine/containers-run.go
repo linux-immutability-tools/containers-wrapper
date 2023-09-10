b5a4725a477431ce653b7c4ae15c09e92a53515b
mirkobrombin <send@mirko.pm>
2023-09-10 10:46:28 +0200

    add  missing interactive flag

--- pkg/cengine/containers-run.go
+++ pkg/cengine/containers-run.go
@@ -6,13 +6,16 @@ import (
 
 // RunInContainer executes a command inside a new container with the specified
 // ContainerExecOptions.
-func (ce *Ce) RunInContainer(imageOrId string, command string) (err error) {
+func (ce *Ce) RunInContainer(imageOrId string, interactive bool, command string) (err error) {
 	args := []string{"run"}
+	if interactive {
+		args = append(args, "-it")
+	}
 	args = append(args, "--rm")
 	args = append(args, "--entrypoint", command)
 	args = append(args, imageOrId)
 
-	exitCode, _, err := ce.RunCommand(args, []string{}, true)
+	exitCode, _, err := ce.RunCommand(args, []string{}, false)
 
 	switch exitCode {
 	case 0:
